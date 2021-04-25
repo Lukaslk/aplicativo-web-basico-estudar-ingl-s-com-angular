d2216e702917a17a6c6adb2b674f56e5fb1ab9f1
Lucas Rodrigues de Oliveira <pro.lukas@hotmail.com>
2021-04-25 12:43:21 -0300

    Aplicativo web básico de estudo de inglês com angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './top/topo.component';
+import { PainelComponent } from './panel/painel.component';
+import { TentativasComponent } from './attempts/tentativas.component';
+import { ProgressoComponent } from './progress/progresso.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
